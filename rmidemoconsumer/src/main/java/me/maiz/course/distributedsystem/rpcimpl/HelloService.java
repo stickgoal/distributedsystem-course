da4255715b55bd089e897bc878ca2bbd1be1795a
lucas <stick.goal@163.com>
2018-11-24 14:43:58 +0800

    增加rpcdemo

--- rmidemoconsumer/src/main/java/me/maiz/course/distributedsystem/rpcimpl/HelloService.java
+++
@@ -1,5 +0,0 @@
-package me.maiz.course.distributedsystem.rpcimpl;
-
-public interface HelloService {
-    String sayHello(String name);
-}
--- rmidemoconsumer/src/main/java/me/maiz/course/distributedsystem/rpcimpl/RMIClient.java
+++
@@ -1,4 +0,0 @@
-package me.maiz.course.distributedsystem.rpcimpl;
-
-public class RMIClient {
-}
